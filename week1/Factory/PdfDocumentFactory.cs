cdd181682dd158ea48ee3442aa3ed1f034a36f47
Soumit <124624401+Soumit2003@users.noreply.github.com>
2025-06-21 19:56:52 +0530

    Add files via upload

---
+++ week1/Factory/PdfDocumentFactory.cs
@@ -0,0 +1,16 @@
+﻿
+using FactoryMethodPatternExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PdfDocumentFactory : DocumentFactory
+{
+    public override IDocument CreateDocument()
+    {
+        return new PdfDocument();
+    }
+}
