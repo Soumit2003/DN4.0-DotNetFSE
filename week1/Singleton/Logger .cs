596c1a198c9c09b90151bb45be45b5c15272bc01
Soumit <124624401+Soumit2003@users.noreply.github.com>
2025-06-21 14:57:17 +0530

    Add files via upload

---
+++ week1/Singleton/Logger .cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class Logger
+    {
+        private static Logger? _instance;
+        private static readonly object _lock = new Logger();
+
+        private Logger() { }
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock) 
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Logger();
+                        }
+                    }
+                }
+                return _instance!;
+            }
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine("[LOG] " + message);
+        }
+    }
+}
