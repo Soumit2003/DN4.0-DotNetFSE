74abaff40ffbf3a84f2e7f3744189288d3a26eb0
Soumit <124624401+Soumit2003@users.noreply.github.com>
2025-06-22 13:24:51 +0530

    Add files via upload

---
+++ week1/Algorithm/E-commerce Platform Search Function/Product .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce_Platform_Search_Function
+{
+    public class Product
+    {
+        public int productId;
+        public string productName;
+        public string category;
+        public Product(int Id,string Name,string Category )
+        {
+            productId = Id;
+            productName = Name;
+            category = Category;
+        }
+    }
+}
