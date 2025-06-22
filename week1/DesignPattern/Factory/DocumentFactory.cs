05029cc16d02317ab2ccf1317fcd4500eb19a91a
Soumit <124624401+Soumit2003@users.noreply.github.com>
2025-06-22 13:37:08 +0530

    Add files via upload

---
+++ week1/DesignPattern/Factory/DocumentFactory.cs
@@ -0,0 +1,14 @@
+﻿
+using FactoryMethodPatternExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class DocumentFactory
+{
+    public abstract IDocument CreateDocument();
+}
+
---
+++ week1/DesignPattern/Factory/ExcelDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPatternExample
+{
+    internal class ExcelDocument :IDocument
+    {
+        public void open() 
+        {
+            Console.WriteLine("Excel Document opened");
+        }
+    }
+}
