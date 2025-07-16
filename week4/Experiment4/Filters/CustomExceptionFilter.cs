7c0cbb09fb96dad5eaccdff0d0bcc81e6ef2494f
Soumit <124624401+Soumit2003@users.noreply.github.com>
2025-07-16 15:37:16 +0530

    Add files via upload

---
+++ week4/Experiment4/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.IO;
+
+namespace Experiment4.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+     
+            var exception = context.Exception;
+            var message = $"[{DateTime.Now}] Exception: {exception.Message}\nStackTrace: {exception.StackTrace}\n\n";
+
+            var logPath = Path.Combine(Directory.GetCurrentDirectory(), "Logs");
+            if (!Directory.Exists(logPath))
+                Directory.CreateDirectory(logPath);
+
+            File.AppendAllText(Path.Combine(logPath, "exceptions.log"), message);
+
+            context.Result = new ObjectResult("An unexpected error occurred.")
+            {
+                StatusCode = 500
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ week4/Experiment4/Models/Skill.cs
@@ -0,0 +1,8 @@
+﻿namespace Experiment4.Models
+{
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
